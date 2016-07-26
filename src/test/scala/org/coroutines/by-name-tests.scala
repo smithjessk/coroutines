570221f550798c66f1edd834fcba2490b1bc1930
Jess smith <smith.jessk@gmail.com>
2016-07-26 16:02:18 -0400

    Pass coroutine applications by-name in ByNameTest
    Previously, the parameters passed by-name were unapplied coroutines.

--- src/test/scala/org/coroutines/by-name-tests.scala
+++ src/test/scala/org/coroutines/by-name-tests.scala
@@ -7,34 +7,34 @@ import org.scalatest._
 
 
 class ByNameTest extends FunSuite with Matchers {
-  test("coroutines should not be allowed as by-name parameters 1") {
+  test("coroutine applications should not be allowed as by-name parameters 1") {
     import scala.concurrent.Future
     import scala.concurrent.ExecutionContext.Implicits.global
 
-    val c1 = coroutine { () => 
-      5 
+    val c1 = coroutine { (i: Int) =>
+      i
     }
     """
     val c2 = coroutine { () =>
-      val f = Future(c1)
+      val f = Future(c1(4))
       10
     }
-    """  shouldNot typeCheck 
+    """  shouldNot typeCheck
   }
 
-  test("coroutines should not be allowed as by-name parameters 2") {
+  test("coroutine applications should not be allowed as by-name parameters 2") {
     def foo(a: Int, b: => Coroutine._0[Nothing, Int], c: Int): Int = {
       val instance = call(b())
       instance.resume 
       a + instance.result + c
     }
 
-    val c1 = coroutine { () =>
-      2
+    val c1 = coroutine { (i: Int) =>
+      i
     }
     """
     val c2 = coroutine { () =>
-      foo(1, c1, 3)
+      foo(1, c1(2), 3)
     }
     """ shouldNot typeCheck
   }
